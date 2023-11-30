4bcb1e9bd6db27374c96d1b03f49b2799f038f34
Mashyow <145169441+Mashyow@users.noreply.github.com>
2023-11-30 22:45:28 +0300

    Add files via upload

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,47 @@
+package web.model;
+
+import java.util.Objects;
+
+public class Car {
+    private final String model;
+
+    private final String type;
+
+    private final Integer gosnomer;
+
+    public Car(String model, String type, Integer gosnomer) {
+        this.model = model;
+        this.type = type;
+        this.gosnomer = gosnomer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getType() {
+        return type;
+    }
+    public Integer getGosnomer() {
+        return gosnomer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(model, car.model)
+                && Objects.equals(type, car.type)
+                && Objects.equals(gosnomer, car.gosnomer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, type, gosnomer);
+    }
+
+    @Override
+    public String toString() {
+        return "Car: model: " + model + ", type: " + type + ", gosnomer: " + gosnomer;
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car("Mercedes", "Maybach", 100));
+        cars.add(new Car("BMW", "G30", 030));
+        cars.add(new Car("Audi", "80", 880));
+        cars.add(new Car("Skoda", "Octavia", 878));
+        cars.add(new Car("Ford", "FordRS", 676));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.stream().limit(Math.max(count, 0)).toList();
+    }
+}
